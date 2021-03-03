c3e4d40038a312e9f03baa0c66d354dfc8e6591c
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2021-03-03 22:27:26 +0200

    grow animation added

--- src/App/Questions/Question/index.jsx
+++ src/App/Questions/Question/index.jsx
@@ -1,27 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "@material-ui/core";
+import { Button, Grow } from "@material-ui/core";
 
 export const Question = ({ question, onOptionClick = () => {} }) => {
   const { options, title, isSubmitted = false, id, selected } = question;
+  const [show, setShow] = useState(true);
+
+  const toggleShow = () => {
+    if (isSubmitted) return;
+    setShow(false);
+    setTimeout(() => {
+      setShow(true);
+    }, 100);
+  };
+
+  useEffect(() => {
+    toggleShow();
+  }, [id]);
   console.log({ question, options, title });
 
   return (
     <div style={{ marginTop: "5rem" }}>
       <h3 className="text-center">{title}</h3>
-      <div className="row">
-        {options.map(({ title = "", id: optionId = 0 }) => (
-          <div className="col-md-6">
-            <Button
-              disabled={isSubmitted && selected !== optionId}
-              style={{ width: "100%", marginTop: "0.5rem" }}
-              onClick={() => onOptionClick(id, optionId)}
-              variant="contained"
-            >
-              {title}
-            </Button>
-          </div>
-        ))}
-      </div>
+      {show && (
+        <div className="row">
+          {options.map(({ title = "", id: optionId = 0 }, index) => (
+            <div className="col-md-6">
+              <Grow in timeout={index * 500}>
+                <Button
+                  disabled={isSubmitted && selected !== optionId}
+                  style={{ width: "100%", marginTop: "0.5rem" }}
+                  onClick={() => onOptionClick(id, optionId)}
+                  variant="contained"
+                >
+                  {title}
+                </Button>
+              </Grow>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
