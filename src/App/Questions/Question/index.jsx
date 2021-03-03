89463629652216e82465f750abcfea0f09ad1a6c
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2021-03-03 22:06:35 +0200

    handle disable after submit

--- src/App/Questions/Question/index.jsx
+++ src/App/Questions/Question/index.jsx
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
 
-export const Question = ({ question }) => {
-  const { options, title } = question;
+export const Question = ({ question, onOptionClick = () => {} }) => {
+  const { options, title, isSubmitted = false, id, selected } = question;
   console.log({ question, options, title });
 
   return (
     <div style={{ marginTop: "5rem" }}>
       <h3 className="text-center">{title}</h3>
       <div className="row">
-        {options.map(({ title = "", id = 0 }) => (
-          <div className="col-md-6 gap-md" style={{ gap: "1rem" }}>
+        {options.map(({ title = "", id: optionId = 0 }) => (
+          <div className="col-md-6">
             <Button
+              disabled={isSubmitted && selected !== optionId}
               style={{ width: "100%", marginTop: "0.5rem" }}
-              onClick={() => onOptionClick(id)}
+              onClick={() => onOptionClick(id, optionId)}
               variant="contained"
             >
               {title}
--- src/App/Questions/index.jsx
+++ src/App/Questions/index.jsx
@@ -2,14 +2,30 @@ import React, { useState, useEffect } from "react";
 import { Question } from "./Question/index.jsx";
 import { Button } from "@material-ui/core";
 
-export const Questions = ({ questions = [] }) => {
+export const Questions = ({ questions: initQuestions = [] }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [questions, setQuestions] = useState(initQuestions);
 
   const nextQuestion = () => setCurrentQuestion(c => c + 1);
   const prevQuestion = () => setCurrentQuestion(c => c - 1);
 
-  const disableNext = currentQuestion === questions.length;
+  const disableNext = currentQuestion === questions.length - 1;
 
+  const submitQustion = (id , selected) => {
+    const prevQuestion = questions.find(question => question.id === id);
+    const newQuestion = {
+      ...prevQuestion,
+      isSubmitted: true,
+      selected
+    };
+    const questionsAfterEdit = questions.map(question => {
+      if (question.id === id) return newQuestion;
+      return question;
+    });
+    setQuestions(questionsAfterEdit);
+  };
+
+  console.log({ questions });
   return (
     <div>
       <div className="d-flex gap-lg">
@@ -30,7 +46,10 @@ export const Questions = ({ questions = [] }) => {
           Next
         </Button>
       </div>
-      <Question question={questions[currentQuestion]} />
+      <Question
+        question={questions[currentQuestion]}
+        onOptionClick={submitQustion}
+      />
     </div>
   );
 };
